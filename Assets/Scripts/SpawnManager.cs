2e1000e5d57022c8c7072448e249094cebbf538c
Umut Arslan <umut_arslan@gmx.de>
2025-05-13 14:15:41 +0200

    Working State

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,14 +1,57 @@
+//using UnityEngine;
+
+//public class PlayerSpawnPoint : MonoBehaviour
+//{
+//    [SerializeField]
+//    private GameObject targetObject;
+//    void Start()
+//    {
+//        this.transform.position = targetObject.transform.position;
+//        this.transform.rotation = targetObject.transform.rotation;
+//        this.transform.Rotate(0, 180, 0, Space.Self);
+
+//    }
+//}
+
 using UnityEngine;
+using Unity.Netcode;
 
 public class PlayerSpawnPoint : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject targetObject;
-    void Start()
-    {
-        this.transform.position = targetObject.transform.position;
-        this.transform.rotation = targetObject.transform.rotation;
-        this.transform.Rotate(0, 180, 0, Space.Self);
+    [SerializeField] private Transform playerSpawn;
+    [SerializeField] private Transform questRigSpawn;
 
+    private void Start()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnPlayerSpawned;
+    }
+
+    private void OnPlayerSpawned(ulong clientId)
+    {
+        GameObject playerObj = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(clientId).gameObject;
+
+        // Check if it's a Quest Rig (by tag)
+        if (playerObj.CompareTag("QuestRig"))
+        {
+            MoveToSpawnPoint(playerObj, questRigSpawn);
+        }
+        // Default to Player Spawn
+        else
+        {
+            MoveToSpawnPoint(playerObj, playerSpawn);
+        }
+    }
+
+    private void MoveToSpawnPoint(GameObject obj, Transform spawnPoint)
+    {
+        obj.transform.position = spawnPoint.position;
+        obj.transform.rotation = spawnPoint.rotation;
+        obj.transform.Rotate(0, 180, 0, Space.Self); // Optional rotation adjustment
+    }
+
+    private void OnDestroy()
+    {
+        if (NetworkManager.Singleton != null)
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnPlayerSpawned;
     }
 }
